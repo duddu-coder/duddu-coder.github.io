76ec29a5984b818a8c00a0e4672153f4bbe5c795
Duddu Coder <dudducoder@protonmail.com>
2020-05-13 20:07:43 +0530

    feat: Add carousel and images
    Infinite scroll with fullscreen support.

---
+++ precache-manifest.bd41c513fe19c8c04cc03176e70fed8f.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4712152f79fcc466ba",
+    "url": "css/2.e0c92e77.css"
+  },
+  {
+    "revision": "43803c90e126a65f8407",
+    "url": "css/app.5cb19b67.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.a45108d3.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.cea99d3e.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.865f928c.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.2267169e.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bac8362e.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.49ae34d4.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.f2a09334.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.12a47ed5.woff2"
+  },
+  {
+    "revision": "5f67f0dcfd9c723dfdb1d3f3c8bbe6a5",
+    "url": "img/fuckin-milkman.42c84334.jpg"
+  },
+  {
+    "revision": "154b8663ab9fdf02721dda0f08a3a569",
+    "url": "img/glass.04d901a1.jpg"
+  },
+  {
+    "revision": "3fb2d14c09ecd7eff135eb071504fcbf",
+    "url": "img/milkrider.7f8638e5.jpg"
+  },
+  {
+    "revision": "888983635e44452812ba9c55fa6fae2c",
+    "url": "img/milksplash.2347db1b.jpg"
+  },
+  {
+    "revision": "95a191a0b57cbdb644a17644261d5997",
+    "url": "index.html"
+  },
+  {
+    "revision": "3a4712152f79fcc466ba",
+    "url": "js/2.3a471215.js"
+  },
+  {
+    "revision": "0406f46fee6f3bda865a",
+    "url": "js/3.0406f46f.js"
+  },
+  {
+    "revision": "e6a30255eb4d019f2cce",
+    "url": "js/4.e6a30255.js"
+  },
+  {
+    "revision": "43803c90e126a65f8407",
+    "url": "js/app.1bb65257.js"
+  },
+  {
+    "revision": "d865b6a0c7fca5373856",
+    "url": "js/vendor.d865b6a0.js"
+  },
+  {
+    "revision": "c38da49806647958a0a1ed79576a37c8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "c9920835d2adb9f6ab6a79a27f220a3a",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "9df9e65a1d2c6df3bd9143666b0e8c63",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "4300c6c136c06abcd781aa18644dd7a3",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "88dbf3d9e8269f1298486d103e22a57c",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "1be07e62226c8f6a7086f132d9be1642",
+    "url": "statics/icons/favicon-128x128.png"
+  },
+  {
+    "revision": "df5ff7f4728c4ef4b951d46b4136906f",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "64aa9e88dd6545f64226c1e2230f66cc",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "d22664397060743b24e8956a5a3fcdec",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "8b9eb314ff597f6f7c7bf8c7a5f717ec",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "1be07e62226c8f6a7086f132d9be1642",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "a1b423855a450ae7adbd9d2a782e0dfb",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "9f33ebc999e51d0dc2975fc573d53a0b",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "a126be0d50ed682c3129e0b8b31f680a",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "02abd24e7c8638e848d96a6bf78d2c27",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "f256ec55be616f32ae486ab7571eb936",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "de8107bed7f087fb443d8e36855e7467",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  }
+]);
